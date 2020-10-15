ff5da94d37226c12a46f72e1ce601de936587a65
DzmitryCavi <dzmitry.malakhouski@tut.by>
2020-10-15 15:09:09 +0300

    feat: hanoi and whatseason

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform( arr ) {
+  const isRule = s => ['--discard-next','--double-next','--discard-prev','--double-prev'].includes(s)
+
+  if(!(arr instanceof Array)) throw Error
+  const result = [...arr]
+  let deleted = 0
+  arr.map((el,i) => {
+    switch(el){
+      case '--discard-next': if(!isRule(result[i-deleted+1])) {result.splice(i-deleted+1,1);  deleted++}
+      case '--double-next': if(!isRule(result[i-deleted+1])) result.splice(i-deleted+1,1,result[i-deleted+1])
+      case '--discard-prev':if(result[i-deleted-1]) {result.splice(i-deleted-1,1); deleted ++}
+      case '--double-prev': if(result[i-deleted-1]) result.splice(i-deleted-1,1,result[i-deleted-1])
+    }
+  })
+  
+ 
+  return result.filter(el => !isRule(el))
 };
+
+// --discard-next исключает следующий за ней элемент исходного массива из преобразованного массива.
+// --discard-prev исключает предшествующий ей элемент исходного массива из преобразованного массива.
+// --double-next удваивает следующий за ней элемент исходного массива в преобразованном массиве.
+// --double-prev удваивает предшествующий ей элемент исходного массива в преобразованном массиве.
--- src/what-season.js
+++ src/what-season.js
@@ -1,5 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
+    if (!date) return 'Unable to determine the time of year!';
+    try { date.getTime(); } catch (e) { throw e; }
+    const month = date.getMonth()
+    switch(true){
+        case month < 2 || month === 11: return 'winter'
+        case month < 5: return 'spring'
+        case month < 8: return 'summer'
+        default: return 'autumn'
+    }
+     
 
 };
